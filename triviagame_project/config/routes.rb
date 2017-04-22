6a713e52481188df131a4465e58b9c306919ac7c
knightman92 <krj221@knights.ucf.edu>
2017-04-21 23:13:15 -0400

    Add files via upload

--- triviagame_project/config/routes.rb
+++ triviagame_project/config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+ 
+  root 'pages#index'
 
-  root 'triviagame#index'
+  # match ':controller(/:action(/:id))(.:format)'
 
-  get 'triviagame/index'
-
+  get 'users/new'
+  get 'pages/index'
+  get 'pages/enter'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
